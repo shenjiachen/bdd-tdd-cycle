1b78b5e248d562f67a6f6f5c915d0b20422441cb
Jiachen <shenjiachen@tamu.edu>
2016-03-25 07:15:09 +0000

    sda

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -65,13 +65,13 @@ class MoviesController < ApplicationController
   end
   
   def similar
-    if(params[:director].nil?)
-      title = Movie.find(params[:id]).title
-      flash[:notice] = "'#{title}'hasd no director info"
-      redirect_to movies_path()
-      return
-    end
-    @movies = Movie.where(director:params[:director])
+  #if(params[:director].nil?)
+    # title = Movie.find(params[:id]).title
+    # flash[:notice] = "'#{title}'hasd no director info"
+    # redirect_to movies_path()
+    # return
+   # end
+   # @movies = Movie.where(director:params[:director])
   end
   
 end
